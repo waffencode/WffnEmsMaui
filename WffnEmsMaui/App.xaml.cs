a8e4abb359e5a3a595bebee0e0e4e83bf64fc76d
waffencode <waffencode@gmail.com>
2023-07-08 14:28:40 +0300

    Add project files.

---
+++ WffnEmsMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WffnEmsMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ WffnEmsMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WffnEmsMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}

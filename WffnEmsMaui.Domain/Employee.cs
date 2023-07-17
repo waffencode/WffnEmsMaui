0ce96494610a2011c290ac3bf6132a16c1786bc6
waffencode <waffencode@gmail.com>
2023-07-17 20:11:02 +1000

    Add unit tests for employee class

--- WffnEmsMaui.Domain/Employee.cs
+++ WffnEmsMaui.Domain/Employee.cs
@@ -1,9 +1,29 @@
-﻿namespace WffnEmsMaui.Domain;
+﻿using System.Text.RegularExpressions;
+
+namespace WffnEmsMaui.Domain;
 
 public class Employee
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public string Name { get; set; } = string.Empty;
+    public Guid Id { get; } = Guid.NewGuid();
+
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            // Regex for name which starts with a capital, e.g. "Test".
+            if (Regex.IsMatch(value, "^[A-Z]+[\\s|-]?[a-zA-Z]+[\\s|-]?[a-zA-Z]+$"))
+            {
+                _name = value;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid name.");
+            }
+        }
+    }
+
     public string Surname { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
--- WffnEmsMaui.Test/EmployeeTest.cs
+++ WffnEmsMaui.Test/EmployeeTest.cs
@@ -5,10 +5,41 @@ namespace WffnEmsMaui.Test
     public class EmployeeTest
     {
         [Fact]
-        public void EmployeeNameStartsByCapital()
+        public void IdShouldNotBeNull()
         {
             Employee employee = new();
-            Assert.Throws<Exception>(() => employee.Name = "name");
+            Assert.NotEqual(Guid.Empty, employee.Id);
+        }
+
+        [Fact]
+        public void ShouldAcceptValidName()
+        {
+            Employee employee = new();
+            var exception = Record.Exception(() => ChangeEmployeeName(employee, "Test"));
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionForInvalidName()
+        {
+            Employee employee = new();
+            var exception = Record.Exception(() => ChangeEmployeeName(employee, "test"));
+            Assert.NotNull(exception);
+        }
+
+        [Fact]
+        public void ShouldReturnValidName()
+        {
+            Employee employee = new()
+            {
+                Name = "Test"
+            };
+            Assert.Equal("Test", employee.Name);
+        }
+
+        private static void ChangeEmployeeName(Employee employee, string name)
+        {
+            employee.Name = name;
         }
     }
 }

eda4f05917a62a99b91264ef6619ff1844d0f07f
waffencode <waffencode@gmail.com>
2023-07-25 03:12:48 +1000

    Add EmployeeCollection class

---
+++ WffnEmsMaui.Domain/EmployeeCollection.cs
@@ -0,0 +1,16 @@
+﻿namespace WffnEmsMaui.Domain;
+
+public class EmployeeCollection
+{
+    public void Add(Employee employee)
+    {
+        List.Add(employee);
+    }
+
+    public List<Employee> List { get; } = new ();
+
+    public Employee Get(int i)
+    {
+        return List.ElementAt(i);
+    }
+}
---
+++ WffnEmsMaui.Test/Domain/EmployeeCollectionTest.cs
@@ -0,0 +1,57 @@
+﻿using Netizine.Enums;
+using WffnEmsMaui.Domain;
+
+namespace WffnEmsMaui.Test.Domain;
+
+public class EmployeeCollectionTest
+{
+    [Fact]
+    public void ShouldAddValidItems()
+    {
+        EmployeeCollection collection = new();
+        Employee employee = new()
+        {
+            Name = "Test",
+            Surname = "Test",
+            Address = new()
+            {
+                Country = Country.UnitedStatesOfAmerica,
+                State = "California",
+                City = "Los Angeles",
+                PostalCode = "000000",
+                Street = "Example st.",
+                House = "0",
+                Apartment = "0"
+            },
+            Phone = "+78008008080",
+            Email = new("example@example.com")
+        };
+        collection.Add(employee);
+        Assert.Contains<Employee>(employee, collection.List);
+    }
+
+    [Fact]
+    public void ShouldReturnValidItems()
+    {
+        EmployeeCollection collection = new();
+        Employee employee = new()
+        {
+            Name = "Test",
+            Surname = "Test",
+            Address = new()
+            {
+                Country = Country.UnitedStatesOfAmerica,
+                State = "California",
+                City = "Los Angeles",
+                PostalCode = "000000",
+                Street = "Example st.",
+                House = "0",
+                Apartment = "0"
+            },
+            Phone = "+78008008080",
+            Email = new("example@example.com")
+        };
+        collection.Add(employee);
+        Assert.Equal(employee, collection.Get(0));
+    }
+}

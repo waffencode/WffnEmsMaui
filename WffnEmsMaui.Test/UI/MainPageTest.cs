dc3e03dd13a86e50e2018fcdeb9bf7b26940a78a
waffencode <waffencode@gmail.com>
2023-08-05 02:15:38 +1000

    Add UI test base class

--- WffnEmsMaui.Test/UI/MainPageTest.cs
+++ WffnEmsMaui.Test/UI/MainPageTest.cs
@@ -1,19 +1,15 @@
 ﻿using OpenQA.Selenium.Appium.Windows;
-using OpenQA.Selenium.Remote;
 
 namespace WffnEmsMaui.Test.UI;
 
-public class MainPageTest
+public class MainPageTest : UiTestBase
 {
     [Fact]
     public void MainPageAccessTest()
     {
-        DesiredCapabilities appCapabilities = new();
-        appCapabilities.SetCapability("app", "35aa53ec-81b4-4442-af4e-db6f33853969_9zz4h110yvjzm!App");
-        var mainPage = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appCapabilities);
-        var button = mainPage.FindElementByAccessibilityId("CounterBtn");
+        WindowsElement? button = Driver.FindElementByAccessibilityId("CounterBtn");
+        Assert.NotNull(button);
         button.Click();
         Assert.Equal("Clicked 1 time", button.Text);
-        mainPage.CloseApp();
     }
 }

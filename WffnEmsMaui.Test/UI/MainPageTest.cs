ec7f4e7bf88b7d55221e3f3dd2a92be67181b926
waffencode <waffencode@gmail.com>
2023-08-05 01:13:11 +1000

    Implement simple UI test approach

---
+++ WffnEmsMaui.Test/UI/MainPageTest.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+
+namespace WffnEmsMaui.Test.UI;
+
+public class MainPageTest
+{
+    [Fact]
+    public void MainPageAccessTest()
+    {
+        DesiredCapabilities appCapabilities = new();
+        appCapabilities.SetCapability("app", "35aa53ec-81b4-4442-af4e-db6f33853969_9zz4h110yvjzm!App");
+        var mainPage = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appCapabilities);
+        var button = mainPage.FindElementByAccessibilityId("CounterBtn");
+        button.Click();
+        Assert.Equal("Clicked 1 time", button.Text);
+    }
+}
